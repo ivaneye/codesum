36c8bee70727cbbe5b886b34b42d2fe5fb04ed27
ivanpig <ivan.pig@foxmail.com>
2016-01-27 16:47:57 +0800

    使用golang flag处理命令行参数

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 使用Go语言编写的代码统计小工具
+执行:
+
+codesum.exe -path={path-to-caculate}
+            -lang={language type:Default is java}
+            -single={Single line comment:Default is //}
+            -prefix={Multi line comment:Default is /\\\\\*}
+            -suffix={Multi line comment:Default is \\\\\*/}
+            -detail={Should show detail:Default is false}
+
--- main.go
+++ main.go
@@ -7,38 +7,31 @@ import (
 	"github.com/ivaneye/codesum/filter"
 	"github.com/ivaneye/codesum/iterator"
 	"fmt"
-	"os"
-	"strconv"
-	"os/exec"
-	"path/filepath"
+	"flag"
 )
 
 func main() {
-	args := os.Args
-	fmt.Println(args[1])
-	if len(args) == 1 {
-		fmt.Println("请输入统计目录!")
+	lang := flag.String("lang", "java", "Language Type:java,go,c...")
+	path := flag.String("path", "", "Path to Caculate")
+	single := flag.String("single", "//", "Single Line Comment")
+	prefix := flag.String("prefix", "/\\*", "Multi Line Comment Prefix")
+	suffix := flag.String("suffix", "\\*/", "Multi Line Comment Suffix")
+	detail := flag.Bool("detail", false, "Should show Detail?")
+	flag.Parse()
+	fmt.Println("lang=", *lang, ",path=", *path, ",single=", *single, ",prefix=", *prefix, ",suffix=", *suffix)
+	if *path == "" {
+		fmt.Println("Please input path to Caculate!")
 		return
 	}
-	conf := initConf(args)
+	conf := initConf(*lang, *path, *single, *prefix, *suffix, *detail)
 	monitor := new(monitor.CmdMonitor)
 	caculator := &caculate.Caculator{conf, monitor, 0}
 	filte := &filter.FileNameFilter{conf.Suffix}
 	iterat := iterator.Iterator{monitor, filte, caculator}
-	iterat.Iterate(args[1])
+	iterat.Iterate(*path)
 	fmt.Println("sum=", caculator.Total)
 }
 
-func initConf(args []string) *conf.Manager {
-	file,_ := exec.LookPath(args[0])
-	path,_ := filepath.Abs(file)
-	fmt.Println("file=",args[0])
-	fmt.Println(path)
-	if len(args) == 2 {
-		//只有目录路径，使用默认配置
-		return &conf.Manager{"//", "/\\*", "\\*/", "java", false}
-	}else {
-		b, _ := strconv.ParseBool(args[6])
-		return &conf.Manager{args[2], args[3], args[4], args[5], b}
-	}
+func initConf(lang, path, single, prefix, suffix string, detail bool) *conf.Manager {
+	return &conf.Manager{single, prefix, suffix, lang, detail}
 }
